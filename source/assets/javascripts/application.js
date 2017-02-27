439b578170bcf2818e9bf88fe949d3d8b67c950b
Giovanni Arnone <info@crazydesigner.it>
2017-02-27 17:50:59 +0100

    inizio sito

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -6,3 +6,22 @@ require('modernizr');
 $(document).ready(() => {
   console.log("Hello world");
 });
+
+$(document).ready(function() {
+  const menu = $(".header__items");
+  const hamburger = $(".header__hamburger");
+
+  $(".header__hamburger").click((e) => {
+	console.log("Hello worldd");
+
+    e.preventDefault();
+    if(menu.hasClass("is-open")) {
+      menu.removeClass("is-open");
+      hamburger.removeClass("expanded");
+
+    } else {
+      menu.addClass("is-open");
+      hamburger.addClass("expanded");
+    }
+  });
+});
